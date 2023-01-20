4632b78921bdcf9310d7b7012fc49c9e46b1b7af
mina <123149457+minhwa518@users.noreply.github.com>
2023-01-20 17:11:38 +0900

    Update imh07.py

--- imh07.py
+++ imh07.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 st.title('Graph Land')
-st.title('_You can do it!_ :#1E8449[We are amasing!] and emojis :sushi:')
+st.title('_You can do it!_ :darkpink[We are here!] and emojis :sushi:')
 
 money = pd.read_csv("money_data7.csv")
 
