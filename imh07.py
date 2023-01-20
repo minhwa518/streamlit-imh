db0da81919aca89de015aa41351a1a7fbfef14e8
mina <123149457+minhwa518@users.noreply.github.com>
2023-01-20 17:21:28 +0900

    Update imh07.py

--- imh07.py
+++ imh07.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 st.title('Graph Land')
-st.title('_Korea - America!_ :green[Money data] :coin:')
+st.title('_Korea - America!_ :green[Money data] :gold:')
 
 money = pd.read_csv("money_data7.csv")
 
