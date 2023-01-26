dc8297bf8845ef3c4654dad9c8a5af346d897d1c
IMH <123149457+minhwa518@users.noreply.github.com>
2023-01-26 17:22:47 +0900

    Update imh07.py

--- imh07.py
+++ imh07.py
@@ -10,7 +10,7 @@ import numpy as np
 
 def  plotting_demo():
     
-    st.title('<IMH :heavy black heart Graph Land>')
+    st.title('<IMH :heavy black heart: Graph Land>')
     st.title('_Korea - America_ :green[Money data] :100:')
     
     money = pd.read_csv("money_data7.csv")
