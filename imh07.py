5c95ce0a207a8d1a35aab155ee5e0ea33992c92f
IMH <123149457+minhwa518@users.noreply.github.com>
2023-02-01 17:05:33 +0900

    Update imh07.py

--- imh07.py
+++ imh07.py
@@ -74,7 +74,7 @@ def bar_chart():
     
     option = st.selectbox(
         'How would you like to choice year ?',
-        ('2015', '2016','2017', '2018', '2019', '2020', '2021', '2022'))
+        ('2022', '2021','2020', '2019', '2018', '2017', '2016', '2015'))
 
     option2 = option
 
