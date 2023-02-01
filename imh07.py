ad6139764172f2ce49a36b217811dcab0f74036b
IMH <123149457+minhwa518@users.noreply.github.com>
2023-02-01 17:16:33 +0900

    Update imh07.py

--- imh07.py
+++ imh07.py
@@ -58,7 +58,7 @@ def  plotting_demo():
 def bar_chart():
 
     st.title('💖Graph Land💖')
-    st.title('_Korea_:brown[Baseball Record] :baseball:')
+    st.title('Korea brown[Baseball Record] :baseball:')
     
     url = "https://sports.news.naver.com/kbaseball/record/index?category=kbo&year="
 
